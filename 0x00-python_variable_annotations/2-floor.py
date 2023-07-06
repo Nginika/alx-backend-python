2a597343c5caf28abbc034259256d67de21688c6
Nginika <nginikaugochukwu@gmail.com>
2023-07-06 16:21:26 +0000

    calculates the floor of a number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+type-annotated function floor which takes a float n
+as argument and returns the floor of the float.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    calculate the float of a number
+    """
+    return math.floor(n)
