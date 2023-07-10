5b9b5807f51e6f91a8df6771c2209808d961871f
Nginika <nginikaugochukwu@gmail.com>
2023-07-10 16:50:04 +0000

    concurrent coroutines sorted by as_completed function of async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,6 +5,7 @@ arguments (in this order): n and max_delay. You will spawn
 wait_random n times with the specified max_delay.
 """
 import asyncio
+import random
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
