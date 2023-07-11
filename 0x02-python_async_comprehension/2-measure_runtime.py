d3abac8b08809b1fc659139cabe58baf591bf7c8
Nginika <nginikaugochukwu@gmail.com>
2023-07-11 18:37:00 +0000

    async comprehension and time taken to run async coroutines in comprehensions

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -13,6 +13,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """measure time for async.gather"""
     s = time.perf_counter()
     await asyncio.gather(
             async_comprehension(),
