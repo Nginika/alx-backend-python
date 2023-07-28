7af94268ccfebde7a1b335988e321d0ca75f5b1a
Nginika <nginikaugochukwu@gmail.com>
2023-07-28 15:25:08 +0000

    unittest module using parameter expand

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""unit test for utils.access_nested_map"""
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """class that inherits from unittest testcase"""
+    def setUp(self):
+        """method invoked for each test"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ])
+    def test_access_nested_map(self, nested_map, path, result):
+        """test that it returns what it should"""
+        self.assertEqual(access_nested_map(nested_map, path), result)
