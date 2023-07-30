9f2f3c7a8691549f88dff3f53d5b817f415636e1
Nginika <nginikaugochukwu@gmail.com>
2023-07-30 18:33:23 +0000

    test the pub repo fn with property mock

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Test for githuborgclient class methods"""
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from utils import access_nested_map, get_json, memoize
 from client import GithubOrgClient
 
@@ -20,3 +20,14 @@ class TestGithubOrgClient(unittest.TestCase):
         example = GithubOrgClient(test_orgname)
         example.org()
         mock_getjson.assert_called_once_with(url)
+
+    @parameterized.expand([
+        ("google", {"repos_url": "https://api.github.com/orgs/google"})
+     ])
+    def test_public_repos_url(self, test_org, response):
+        """test with the property get mock method"""
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as tester:
+            tester.return_value = response
+            example = GithubOrgClient(test_org)._public_repos_url
+            self.assertEqual(example, response.get("repos_url"))
